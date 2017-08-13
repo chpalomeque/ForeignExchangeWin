1abe5e1cfcba60c8cd004e5763d7d5beff79ce1a
ch palomequer <christianpalomeque1989@gmail.com>
2017-08-13 18:53:30 -0500

    more changes

--- ForeignExchangeWin/ForeignExchangeWin/ForeignExchangeWin/MainPage.xaml.cs
+++ ForeignExchangeWin/ForeignExchangeWin/ForeignExchangeWin/MainPage.xaml.cs
@@ -12,6 +12,32 @@ namespace ForeignExchangeWin
         public MainPage()
         {
             InitializeComponent();
+
+            ConverButton.Clicked += ConverButton_Clicked;    
+        }
+
+        private void ConverButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(PesosEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in pesos.", "Acept");
+                return;
+            }
+
+            decimal pesos = 0;
+            if (decimal.TryParse(PesosEntry.Text, out pesos))
+            {
+                DisplayAlert("Error", "You must enter a value numeric in pesos ..", "Accept");
+            }
+
+            var dollars = pesos / 3003.003M;
+            var euros   = pesos / 3531.05105M;
+            var pounds  = pesos / 3907.23724M;
+
+            DollarsEntry.Text = string.Format("{0:C2}", dollars);
+            EurosEntry.Text   = string.Format("{0:C2}", euros);
+            PoundsEntry.Text  = string.Format("{0:C2}", pounds);
+
         }
     }
 }
